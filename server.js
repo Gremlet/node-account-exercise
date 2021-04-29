15c1cd354d340abe71ca4d9588850a2fb17ae12c
Gremlet <57172450+Gremlet@users.noreply.github.com>
2021-04-29 15:01:34 +0200

    fix imports

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express')
-const lowdb = require('lowdb')
 const port = 8080
-const FileSync = require('lowdb/adapters/FileSync')
 const { initialiseDatabase } = require('./dbHandlers')
-const adapter = new FileSync('accounts.json')
 
 const accountRouter = require('./routes/index')
-
-const db = lowdb(adapter)
 
 const app = express()
 
